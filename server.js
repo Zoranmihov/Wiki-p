8560d05f15bf0141e5eae4deb73b8df48b38a7fa
Zoran Mihov <65128667+Zoranmihov@users.noreply.github.com>
2022-04-12 10:00:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ let articlesRoute = require("./routes/articlesRoute");
 app.use("/api/users/", userRoute);
 app.use("/api/articles/", articlesRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
 app.use((req, res, next) => {
   const error = new Error("Page not found");
   error.status = 404;
@@ -24,13 +31,6 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   res.status(error.status || 500).json({ error: { message: error.message } });
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000;
 
